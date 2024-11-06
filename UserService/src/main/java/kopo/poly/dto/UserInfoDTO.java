266275e985f4e994427efdbc4ff61e895ed36242
lee-jae-o <dlwodh8746@naver.com>
2024-11-06 09:15:52 +0900

    commit

---
+++ UserService/src/main/java/kopo/poly/dto/UserInfoDTO.java
@@ -0,0 +1,18 @@
+package kopo.poly.dto;
+
+import lombok.Builder;
+
+@Builder
+public record UserInfoDTO(
+        String userId,
+        String userName,
+        String password,
+        String email,
+        String addr1,
+        String addr2,
+        String regId,
+        String regDt,
+        String chgId,
+        String chgDt,
+        String roles
+){}
